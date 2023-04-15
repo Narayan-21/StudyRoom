07dd5f9d2937ff468a9dbe94384b9a78c95f12e9
Narayan-21 <naaidjan.19@gmail.com>
2023-04-15 16:51:14 +0530

    Search Bar

--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 from django.http import HttpResponse
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -14,9 +15,14 @@ from .forms import RoomForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains = q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains = q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics': topics}
+    room_count = rooms.count()
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 

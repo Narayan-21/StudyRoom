eb703f4e73f892ad159428b3a410d55413f0e465
Narayan-21 <naaidjan.19@gmail.com>
2023-04-21 18:17:39 +0530

    Adding basic Django REST Framework

---
+++ base/api/serializers.py
@@ -0,0 +1,9 @@
+# To convert python objects into JSON/javascript object
+from rest_framework.serializers import  ModelSerializer
+from base.models import Room
+
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
